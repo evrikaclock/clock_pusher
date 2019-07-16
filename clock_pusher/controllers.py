ee3260a33d76f9989af7eeec2a16891e2c513dab
Svalova Nastya <svalovaaa@skbkontur.ru>
2019-07-16 21:14:51 +0500

    release_day

--- clock_pusher/controllers.py
+++ clock_pusher/controllers.py
@@ -1,6 +1,7 @@
 from clock_pusher import app
 import json
 import time
+import datetime
 
 
 @app.websocket('/')
@@ -17,7 +18,15 @@ async def sync(_, ws):
                 print(e)
                 time.sleep(5)
         if int(state["revision"]) > current_revision:
+            release_day = datetime.datetime.strptime(state["release_day"], "%d.%m.%Y %X")
+            now = datetime.datetime.now()
+            total_seconds = int((now - release_day).total_seconds())
+
             current_revision = int(state["revision"])
-            s_state = json.dumps(state)
+            s_state = json.dumps({
+                "total_seconds": total_seconds,
+                "last_service": state["last_service"],
+                "services_count": state["services_count"]
+            })
             await ws.send(s_state)
         time.sleep(1)
